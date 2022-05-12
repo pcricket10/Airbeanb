28dbf9acf884f1bc3feca57f8014acb63051c0e2
Philip McCrickard <pcricket10@gmail.com>
2022-05-12 19:25:12 -0400

    home page styles

--- react-app/src/components/Posts/index.js
+++ react-app/src/components/Posts/index.js
@@ -13,7 +13,25 @@ function Posts() {
     dispatch(getPosts())
   }, [dispatch])
   return (
-    <h1>hello from posts</h1>
+    <><h1>hello from posts</h1>
+      <p>hard coded. change later</p>
+      <div className="bean-feed" >
+
+
+        <img className="bean" src="https://solidstarts.com/wp-content/uploads/Green-Bean-scaled.jpg"></img>
+        <img className="bean" src="https://www.eatthis.com/wp-content/uploads/sites/4/2020/03/variety-of-beans.jpg?quality=82&strip=1"></img>
+        <img className="bean" src="https://solidstarts.com/wp-content/uploads/Green-Bean-scaled.jpg"></img>
+        <img className="bean" src="https://www.eatthis.com/wp-content/uploads/sites/4/2020/03/variety-of-beans.jpg?quality=82&strip=1"></img>
+        <img className="bean" src="https://solidstarts.com/wp-content/uploads/Green-Bean-scaled.jpg"></img>
+        <img className="bean" src="https://www.eatthis.com/wp-content/uploads/sites/4/2020/03/variety-of-beans.jpg?quality=82&strip=1"></img>
+        <img className="bean" src="https://solidstarts.com/wp-content/uploads/Green-Bean-scaled.jpg"></img>
+        <img className="bean" src="https://www.eatthis.com/wp-content/uploads/sites/4/2020/03/variety-of-beans.jpg?quality=82&strip=1"></img>
+        <img className="bean" src="https://solidstarts.com/wp-content/uploads/Green-Bean-scaled.jpg"></img>
+        <img className="bean" src="https://www.eatthis.com/wp-content/uploads/sites/4/2020/03/variety-of-beans.jpg?quality=82&strip=1"></img>
+        <img className="bean" src="https://solidstarts.com/wp-content/uploads/Green-Bean-scaled.jpg"></img>
+        <img className="bean" src="https://www.eatthis.com/wp-content/uploads/sites/4/2020/03/variety-of-beans.jpg?quality=82&strip=1"></img>
+      </div>
+    </>
 
   )
 }
