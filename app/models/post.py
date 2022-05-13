e69315e19c1880e68bcb59ab8db2cbe823d3e268
Philip McCrickard <pcricket10@gmail.com>
2022-05-13 17:44:49 -0400

    added img_url to posts model

--- app/models/post.py
+++ app/models/post.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 from .db import db
 
+
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     product_name = db.Column(db.String(50), nullable=False)
+    img_url = db.Column(db.String(255), nullable=False)
     price = db.Column(db.Float(precision=2), nullable=False)
     created_at = db.Column(
         db.DateTime(), default=datetime.utcnow, nullable=False)
@@ -16,6 +18,7 @@ class Post(db.Model):
         return {
             'id': self.id,
             'product_name': self.product_name,
+            'img_url': self.img_url,
             'created_at': self.created_at,
             'updated_at': self.updated_at
         }
