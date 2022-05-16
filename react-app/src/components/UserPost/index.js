4edd02e61c25e3fae1352d883471e25546864677
Philip McCrickard <pcricket10@gmail.com>
2022-05-16 19:37:00 -0400

    create and delete function

--- react-app/src/components/UserPost/index.js
+++ react-app/src/components/UserPost/index.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux'
-import { NavLink, useParams } from 'react-router-dom'
-import { getOnePost } from '../../store/posts'
+import { NavLink, useParams, useHistory } from 'react-router-dom'
+import { getOnePost, deletePost } from '../../store/posts'
 import "./UserPost.css"
+
 
 function UserPost() {
   const dispatch = useDispatch();
+  const history = useHistory();
   // const user = useSelector(state => state.session.user)
   const posts = useSelector(state => Object.values(state.posts))
   const { postId } = useParams()
@@ -13,19 +15,32 @@ function UserPost() {
   // console.log("post", post)
   const currentPost = posts[postId - 1]
   console.log(currentPost, "current post")
+  const handleEdit = async (e) => {
+    e.preventDefault();
 
+    console.log("edit")
+  }
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    const response = await dispatch(deletePost(postId))
+    history.push('/')
+    console.log("delete")
+  }
 
 
   useEffect(() => {
     dispatch(getOnePost(postId))
   }, [dispatch])
+
   return (
-    <>
+    <div className="post-modal">
 
       <h1>{currentPost?.product_name}</h1>
       <img className="bean" src={currentPost?.img_url} alt={currentPost?.product_name} />
       <p>${currentPost?.price} </p>
-    </>
+      <button className="edit-button" onClick={handleEdit}>Edit</button>
+      <button className="delete-button" onClick={handleDelete}>Delete</button>
+    </div>
 
 
 
