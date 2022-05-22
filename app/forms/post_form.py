3ad59db2f144ae6b500864de82e7ada952d9f7fa
Philip McCrickard <pcricket10@gmail.com>
2022-05-22 18:36:11 -0400

    amount

--- app/forms/post_form.py
+++ app/forms/post_form.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField
-from wtforms.validators import InputRequired, URL, Length
+from wtforms.validators import InputRequired, DataRequired, URL, Length
 
 # def check_length(length, max):
 
@@ -10,7 +10,7 @@ class PostForm(FlaskForm):
     product_name = StringField("product_name", validators=[
         InputRequired(message='⚠️ Please enter a product name'),
         Length(max=50, message="⚠️ The number of characters must be less than 50 characters in length")])
-    price = FloatField("price", validators=[InputRequired(
+    price = FloatField("price", validators=[DataRequired(
         message="⚠️ Please enter a valid amount")])
     img_url = StringField("img_url", validators=[
         InputRequired(message='⚠️ Please enter an image url'), URL(
