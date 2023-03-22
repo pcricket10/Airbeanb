c77d17d1dd8196409df59869a09e9e250b964498
Philip McCrickard <pcricket10@gmail.com>
2023-03-22 16:15:01 -0400

    css

--- react-app/src/components/PostFeed/index.js
+++ react-app/src/components/PostFeed/index.js
@@ -18,17 +18,19 @@ function PostFeed() {
       <ul className="bean-feed" >
         {posts.map(post => (
           <li className="post-container" key={post.id}>
-            <div>
-              <h2 className="product-name">{post.product_name}</h2>
-              <NavLink to={`/posts/${post.id}`} className="bean-link">
-                <img className="bean-homepage" src={post.img_url}
-                  onError={({ currentTarget }) => {
-                    currentTarget.onerror = null;
-                    currentTarget.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/800px-Question_mark_%28black%29.svg.png"
-                  }}
-                  alt={post.product_name}></img>
-              </NavLink>
-            </div>
+
+
+            <NavLink to={`/posts/${post.id}`} className="bean-link">
+              <img className="bean-homepage" src={post.img_url}
+                onError={({ currentTarget }) => {
+                  currentTarget.onerror = null;
+                  currentTarget.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/800px-Question_mark_%28black%29.svg.png"
+                }}
+                alt={post.product_name}></img>
+            </NavLink>
+            <h2 className="product-name">{post.product_name}</h2>
+            <p className="product-location">{post.location}</p>
+            <p className="product-price">$ {post.price}</p>
           </li>
         ))}
       </ul>
