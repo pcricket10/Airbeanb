95eabd3c8c6a695a64a67538c5ed9b9fce84aeb7
Philip McCrickard <pcricket10@gmail.com>
2023-04-05 18:19:01 -0400

    initial hamburger menu, functional but ugly

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import Popup from 'reactjs-popup';
@@ -9,58 +9,53 @@ import PostForm from './PostForm';
 
 const NavBar = ({ isLoaded }) => {
   const sessionUser = useSelector(state => state.session.user);
-
+  const [menuOpen, setMenuOpen] = useState('false');
+  const handleMenuClick = e => {
+    setMenuOpen(!menuOpen)//toggle
+  }
   return (
     <nav>
       <ul className='nav-button-container'>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            <button className='nav-button'> Home</button>
-          </NavLink>
-        </li>
-        {!sessionUser && (
-          <>
-
-            <li>
-              <NavLink to='/login' exact={true} activeClassName='active'>
-                <button className='nav-button'>Login </button>
-              </NavLink>
-
-            </li>
-            <li>
-              <NavLink to='/sign-up' exact={true} activeClassName='active'>
-                <button className='nav-button'>Sign Up </button>
-              </NavLink>
-
-            </li>
-          </>
-        )}
-        {sessionUser && (
-          <>
-            {/* <li>
-              <NavLink to='/users' exact={true} activeClassName='active'>
-                <button className='nav-button'> Users </button>
-              </NavLink>
-            </li> */}
-
-            <li>
-              {/* <NavLink to='/posts/new'>
-                <button className='nav-button'>New</button>
-              </NavLink> */}
-              <Popup trigger={<button className='nav-button'>New</button>}
-                modal nested>
-                {close => <PostForm close={close} />}
-              </Popup>
-            </li>
-
-            <li>
-              <LogoutButton />
-            </li>
-          </>
-        )}
+        <NavLink to='/' exact={true} activeClassName='active'>
+          <button className='nav-button'> Home</button>
+        </NavLink>
+        <div className='hamburger-menu' onClick={handleMenuClick}><p>MENU</p>
+          {menuOpen && (
+            <ul>
+              {!sessionUser && (
+                <>
+                  <li>
+                    <NavLink to='/login' exact={true} activeClassName='active'>
+                      <button className='nav-button'>Login </button>
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                      <button className='nav-button'>Sign Up </button>
+                    </NavLink>
+                  </li>
+                </>
 
 
+              )}
+              {sessionUser && (
+                <>
+                  <li>
 
+                    <Popup trigger={<button className='nav-button'>New</button>}
+                      modal nested>
+                      {close => <PostForm close={close} />}
+                    </Popup>
+                  </li>
+
+                  <li>
+                    <LogoutButton />
+                  </li>
+                </>
+              )}
+            </ul>
+          )}
+        </div>
       </ul>
     </nav>
   );
