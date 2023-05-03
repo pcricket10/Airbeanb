861e86b5f77c3f6a37400646f3198b05b03d001f
Philip McCrickard <pcricket10@gmail.com>
2023-05-03 14:31:54 -0400

    at least the menu doesn't shift when opening

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -9,54 +9,46 @@ import PostForm from './PostForm';
 
 const NavBar = ({ isLoaded }) => {
   const sessionUser = useSelector(state => state.session.user);
-  const [menuOpen, setMenuOpen] = useState('false');
+  const [menuOpen, setMenuOpen] = useState(false);
   const handleMenuClick = e => {
     setMenuOpen(!menuOpen)//toggle
   }
   return (
     <nav>
-      <ul className='nav-button-container'>
-        <NavLink to='/' exact={true} activeClassName='active'>
-          <button className='nav-button'> Home</button>
-        </NavLink>
+      <div className='nav-wrapper'>
+        <div className='nav-button-container'>
+          <NavLink to='/' exact={true} activeClassName='active'>
+            <button className='nav-button'> Home</button>
+          </NavLink>
+        </div>
         <div className='hamburger-menu' onClick={handleMenuClick}><p>MENU</p>
           {menuOpen && (
-            <ul>
+            <>
               {!sessionUser && (
                 <>
-                  <li>
-                    <NavLink to='/login' exact={true} activeClassName='active'>
-                      <button className='nav-button'>Login </button>
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink to='/sign-up' exact={true} activeClassName='active'>
-                      <button className='nav-button'>Sign Up </button>
-                    </NavLink>
-                  </li>
+                  <NavLink to='/login' exact={true} activeClassName='active'>
+                    <button className='nav-button'>Login </button>
+                  </NavLink>
+                  <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                    <button className='nav-button'>Sign Up </button>
+                  </NavLink>
                 </>
-
 
               )}
               {sessionUser && (
                 <>
-                  <li>
 
-                    <Popup trigger={<button className='nav-button'>New</button>}
-                      modal nested>
-                      {close => <PostForm close={close} />}
-                    </Popup>
-                  </li>
-
-                  <li>
-                    <LogoutButton />
-                  </li>
+                  <Popup trigger={<button className='nav-button'>New</button>}
+                    modal nested>
+                    {close => <PostForm close={close} />}
+                  </Popup>
+                  <LogoutButton />
                 </>
               )}
-            </ul>
+            </>
           )}
         </div>
-      </ul>
+      </div>
     </nav>
   );
 }
